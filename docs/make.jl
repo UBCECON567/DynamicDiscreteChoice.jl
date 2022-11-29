c9e5fc226999ecbda1e83560d672e3de90f436f5
Paul Schrimpf <paul.schrimpf@gmail.com>
2022-11-29 13:50:05 -0800

    updates to docs packages

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ bib = CitationBibliography("ddc.bib")
 
 makedocs(bib;
     modules=[DynamicDiscreteChoice],
-    authors="Paul Schrimpf <paul.schrimpf@gmail.com> and contributors",
+    authors="Paul Schrimpf <paul.schrimpf@ubc.ca> and contributors",
     repo="https://github.com/ubcecon567/DynamicDiscreteChoice.jl/blob/{commit}{path}#{line}",
     sitename="DynamicDiscreteChoice.jl",
     format=Documenter.HTML(;
@@ -25,3 +25,7 @@ deploydocs(;
     repo="github.com/ubcecon567/DynamicDiscreteChoice.jl",
     devbranch="main",
 )
+
+
+# add pluto html as in https://github.com/JuliaManifolds/Manopt.jl/blob/00aa4a8f833b4052bfa97ca8bcf7956c3286639c/docs/make.jl#L62-L64
+# or https://github.com/fonsp/Pluto.jl/discussions/1345
